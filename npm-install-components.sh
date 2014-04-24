bd6798d63455bf43727875f68ce8313b33e5301a
Adam Hooper <adam@adamhooper.com>
2014-04-24 12:41:00 -0400

    Tweaks

--- npm-install-components.sh
+++ npm-install-components.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 # This script is used by awsbox to install dependencies for all apps.
+#
+# You may also use it if you're just starting out with this dev
+# environment.
 
 (cd editor && npm install)
