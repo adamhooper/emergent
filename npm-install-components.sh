7fd3399af2a2d6d640b98905fe44390f490ada5d
Adam Hooper <adam@adamhooper.com>
2014-04-24 12:57:43 -0400

    Use npm install --production for deploy

--- npm-install-components.sh
+++ npm-install-components.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 # This script is used by awsbox to install dependencies for all apps.
-#
-# You may also use it if you're just starting out with this dev
-# environment.
 
-(cd editor && npm install)
+(cd editor && npm install --production)
