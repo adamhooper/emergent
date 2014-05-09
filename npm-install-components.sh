f2dda6e2acf3560abc3c206ffa7e6cfeaed00f23
Adam Hooper <adam@adamhooper.com>
2014-05-09 15:21:15 -0400

    Do not install bower devDependencies in production

--- npm-install-components.sh
+++ npm-install-components.sh
@@ -2,5 +2,5 @@
 
 # This script is used by awsbox to install dependencies for all apps.
 
-(cd editor && bower install)
+(cd editor && bower install --production)
 (cd editor && npm install --production)
