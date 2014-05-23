9640fc635ac3f59c936be81097f8a362d28cde1e
Adam Hooper <adam@adamhooper.com>
2014-05-23 09:14:15 -0400

    awsbox fix

---
+++ bin/npm-install-components.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This script is used by awsbox to install dependencies for all apps.
+
+NPM_ARGS="--production --cache-min 1209600" # 2 weeks
+
+(cd editor && bower install --production)
+(cd editor && npm install $NPM_ARGS)
+(cd popularity-fetcher && npm install $NPM_ARGS)
--- npm-install-components.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# This script is used by awsbox to install dependencies for all apps.
-
-(cd editor && bower install --production)
-(cd editor && npm install --production)
