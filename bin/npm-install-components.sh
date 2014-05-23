ed43e047c8027b4970c29bb9f847c08b7c21f6e4
Adam Hooper <adam@adamhooper.com>
2014-05-23 14:21:01 -0400

    Try another thing for deploy

--- bin/npm-install-components.sh
+++ bin/npm-install-components.sh
@@ -7,5 +7,4 @@ NPM_ARGS="--production --cache-min 1209600" # 2 weeks
 (cd editor && bower install --production)
 (cd editor && npm install $NPM_ARGS)
 (cd popularity-fetcher && npm install $NPM_ARGS)
-
-npm install -g kexec --cache-min 1209600
+(cd bin && npm install --cache-min 1209600 kexec)
