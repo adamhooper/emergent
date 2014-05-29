b4e88cbf6e66bcbbc8cf50986113da1d83288a23
Adam Hooper <adam@adamhooper.com>
2014-05-29 12:29:10 -0400

    Make first visualization deploy. [refs #6]

--- bin/npm-install-components.sh
+++ bin/npm-install-components.sh
@@ -6,5 +6,6 @@ NPM_ARGS="--production --cache-min 1209600" # 2 weeks
 
 (cd editor && bower install --production)
 (cd editor && npm install $NPM_ARGS)
+(cd viewer && npm install $NPM_ARGS)
 (cd popularity-fetcher && npm install $NPM_ARGS)
 (cd bin && npm install --cache-min 1209600 kexec)
---
+++ bin/viewer.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+process.chdir(__dirname + "/../viewer")
+require('kexec')('npm start')
