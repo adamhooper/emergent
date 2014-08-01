ccaa85fb2af24dd9fc6a6d7c3046b681c2138006
Adam Hooper <adam@adamhooper.com>
2014-08-01 17:38:34 -0400

    More AWS deployment fixes

--- deploy/install/install-postgres.sh
+++ deploy/install/install-postgres.sh
@@ -5,10 +5,11 @@ set -x
 
 sudo yum -y install postgresql postgresql-server postgresql-devel
 sudo service postgresql initdb
+sudo sed -i -e 's/ident$/md5/' /var/lib/pgsql9/data/pg_hba.conf
 sudo chkconfig postgresql on
 sudo service postgresql start
 
 cat <<EOT | sudo su postgres -c psql
-CREATE USER truthmaker WITH PASSWORD 'truthmaker';
-CREATE DATABASE truthmaker OWNER truthmaker;
+CREATE USER truthmaker PASSWORD 'truthmaker';
+CREATE DATABASE truthmaker OWNER truthmaker ENCODING 'utf-8';
 EOT
