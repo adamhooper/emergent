43e7ab26cf7e280a202182c5e1ca205abd4d66a1
Adam Hooper <adam@adamhooper.com>
2014-08-01 16:54:09 -0400

    Fix up npm-install-components.sh

--- bin/npm-install-components.sh
+++ bin/npm-install-components.sh
@@ -4,8 +4,9 @@
 
 NPM_ARGS="--production --cache-min 1209600" # 2 weeks
 
-(cd editor && bower install --production)
+(cd data-store && npm install $NPM_ARGS)
 (cd editor && npm install $NPM_ARGS)
-(cd viewer && npm install $NPM_ARGS)
 (cd url-fetcher && npm install $NPM_ARGS)
+(cd viewer && npm install $NPM_ARGS)
+(cd editor && bower install --production)
 (cd bin && npm install --cache-min 1209600 kexec)
