d9ac6a18ce3d7761c2ef787a6d6b30f0c426a6f8
Adam Hooper <adam@adamhooper.com>
2014-08-01 16:53:33 -0400

    Migrate database after deploy

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 (cd "$(dirname "$0")"/.. && bin/npm-install-components.sh)
+(cd "$(dirname "$0")"/../data-store && env NODE_ENV=production bin/sequelize db:migrate)
 (cd "$(dirname "$0")"/../viewer && node_modules/.bin/gulp --require coffee-script/register prod)
