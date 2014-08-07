96552b8600c5a8a4ebade7c17a2553eebf07f07b
Adam Hooper <adam@adamhooper.com>
2014-08-07 11:34:13 -0400

    Compile assets on deploy

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -2,4 +2,5 @@
 
 (cd "$(dirname "$0")"/.. && bin/npm-install-components.sh)
 (cd "$(dirname "$0")"/../data-store && env NODE_ENV=production bin/sequelize db:migrate)
+(cd "$(dirname "$0")"/../editor && node_modules/.bin/gulp --require coffee-script/register prod)
 #(cd "$(dirname "$0")"/../viewer && node_modules/.bin/gulp --require coffee-script/register prod)
