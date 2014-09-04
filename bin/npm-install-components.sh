987e8fe8b8763807bf7fcdcd23427a4613247fef
Adam Hooper <adam@adamhooper.com>
2014-09-04 17:23:33 -0400

    Automate packaging during deployment

--- bin/npm-install-components.sh
+++ bin/npm-install-components.sh
@@ -7,6 +7,6 @@ NPM_ARGS="--production --cache-min 1209600" # 2 weeks
 (cd data-store && npm install $NPM_ARGS)
 (cd editor && npm install $NPM_ARGS)
 (cd url-fetcher && npm install $NPM_ARGS)
-(cd viewer && npm install $NPM_ARGS)
+#(cd viewer && npm install $NPM_ARGS)
 (cd editor && bower install --production)
 (cd bin && npm install --cache-min 1209600 kexec)
--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -2,5 +2,3 @@
 
 (cd "$(dirname "$0")"/.. && bin/npm-install-components.sh)
 (cd "$(dirname "$0")"/../data-store && env NODE_ENV=production bin/sequelize db:migrate)
-(cd "$(dirname "$0")"/../editor && node_modules/.bin/gulp --require coffee-script/register prod)
-#(cd "$(dirname "$0")"/../viewer && node_modules/.bin/gulp --require coffee-script/register prod)
