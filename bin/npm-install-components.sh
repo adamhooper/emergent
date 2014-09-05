892bed8ff3c28772260a1d94315cdcb60a4dddea
Adam Hooper <adam@adamhooper.com>
2014-09-05 09:05:03 -0400

    No need to run Bower on production...

--- bin/npm-install-components.sh
+++ bin/npm-install-components.sh
@@ -8,5 +8,5 @@ NPM_ARGS="--production --cache-min 1209600" # 2 weeks
 (cd editor && npm install $NPM_ARGS)
 (cd url-fetcher && npm install $NPM_ARGS)
 #(cd viewer && npm install $NPM_ARGS)
-(cd editor && bower install --production)
+#(cd editor && bower install --production)
 (cd bin && npm install --cache-min 1209600 kexec)
