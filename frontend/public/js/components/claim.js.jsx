ba4c96708920dade818326cbb6e8e0c0e9887d12
Adam Hooper <adam@adamhooper.com>
2015-02-19 11:45:30 -0500

    Remove barchart. It relies on an obsolete API.
    [refs #128]

--- frontend/public/js/components/claim.js.jsx
+++ frontend/public/js/components/claim.js.jsx
@@ -52,10 +52,6 @@ module.exports = React.createClass({
     }
   },
 
-  componentDidMount: function() {
-    this.setState({ barChartWidth: 926 });
-  },
-
   setFilter: function(filter) {
     if (filter) {
       var heights = { 'for': 0, against: 0, observing: 0 };
@@ -104,10 +100,6 @@ module.exports = React.createClass({
     return new String(str).replace(/(\d)(?=(\d{3})+$)/g, '$1,');
   },
 
-  truncateString: function(str) {
-    return str.length > 70 ? str.substring(0, str.lastIndexOf(' ', 70)) + '...' : str;
-  },
-
   buildTrackingOnClick: function(type, name) {
     return function() { ga('send', 'event', type, name); };
   },
@@ -116,65 +108,9 @@ module.exports = React.createClass({
     var linker = new Autolinker();
     var claim = this.state.claim;
     var shares = claim.sharesByStance();
-    var colors = [
-      '#00bf71',
-      '#e70909',
-      '#656565'
-    ];
-    var totalShares = _.reduce(shares, function(sum, num) { return sum + num; }, 0);
 
-    if (this.state.populated) {
-      var slices = claim.aggregateSlices().slice(0, 18);
-      var data = slices.reduce(function(series, slice) {
-        series[0].push((slice.for||0) * this.state.filterHeights.for);
-        series[1].push((slice.against||0) * this.state.filterHeights.against);
-        series[2].push((slice.observing||0) * this.state.filterHeights.observing);
-        return series;
-      }.bind(this), [[],[],[]]);
-
-      // find x-axis labels
-      var skips = 3;
-      if (slices.length > 2 * this.state.barChartWidth / 100) { skips = 6; }
-      if (slices.length > 4 * this.state.barChartWidth / 100) { skips = 12; }
-      var labels = slices.map(function(slice, i) {
-        return i%skips ? '' : moment(slice.time).format('MMM Do, YYYY').toUpperCase();
-      });
-      labels[0] = labels[1] = labels[labels.length - 1] = labels[labels.length - 2] = '';
-      // find y-axis labels
-      var highest = _.max(data.reduce(function(heights, line) {
-        line.forEach(function(amount, i) {
-          heights[i] = (heights[i]||0) + amount;
-        });
-        return heights;
-      }, []));
-      var steps = slices.length ? Math.pow(10, Math.floor(Math.log(highest)/Math.log(10) - 0.15)) : 0;
-      var ylabels = _.range(steps, highest, steps);
-      if (ylabels.length > 8) {
-        ylabels = ylabels.filter(function(a,i) { return i%2; });
-      }
-      ylabels = _.reduce(ylabels, function(ylabels, y) {
-        return ylabels.concat({ y: y, label: this.formatNumber(y) });
-      }, [{ y:0, label: 'Total shares' }], this);
-
-      // place callout for confirmation
-      var callout;
-      if (claim.get('truthinessDate')) {
-        callout = {
-          position: _.find(_.range(slices.length), function(s) { return slices[s].time > new Date(claim.get('truthinessDate')); }),
-          text: [
-            moment(claim.get('truthinessDate')).format('MMM Do').toUpperCase(),
-            'CONFIRMED',
-            claim.get('truthiness').toUpperCase()
-          ]
-        };
-      }
-      window.callout = callout;
-    }
-
-    var mostShared = claim.mostShared(),
-      startedTracking = claim.startedTracking(),
-      originDate = claim.originDate(),
-      truthinessDate = claim.get('truthinessDate');
+    var originDate = claim.originDate(),
+        truthinessDate = claim.get('truthinessDate');
 
     var lastDate;
 
@@ -324,14 +260,6 @@ module.exports = React.createClass({
               <div className="page-content">
                 <div className="container">
                   <div className="section section-with-sidebar">
-                    {/*<section className="shares-over-time">
-                      <div className="section">
-                        <h3 className="section-title">Shares Over Time</h3>
-                        <div className="section-content">
-                          <Barchart width={this.state.barChartWidth - 120} height={200} ref="chart" marginBottom={20} marginTop={callout ? 75: 10} marginLeft={100} marginRight={20} ylabels={ylabels} labels={labels} series={data} colors={colors} fontSize={12} gap={0.6} callout={callout} color="#252424"/>
-                        </div>
-                      </div>
-                    </section>*/}
                     <section className="page-timeline">
                       <ul className="articles">
                         {claim.articlesByStance(this.state.filter).map(function(article) {
